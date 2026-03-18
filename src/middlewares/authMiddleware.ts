447bf7a8e4872921449318a050e622da5877dce7
Đoàn Nam Sơn <namson1821@gmail.com>
2026-03-19 03:44:07 +0700

    Fix missing NextFunction import in authMiddleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { jwtConfig } from "../config";
 import UserModel from "../modules/users/user.model";
